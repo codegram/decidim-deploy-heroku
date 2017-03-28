3fc668eec3788ee5260989f3f62416b7c42bfd72
Marc Riera Casals <mrc2407@gmail.com>
2017-03-28 12:32:19 +0200

    Set defaults for review apps

--- lib/generators/heroku_installer.rb
+++ lib/generators/heroku_installer.rb
@@ -9,6 +9,25 @@ module Decidim
         template "app.json.erb", "app.json"
       end
 
+      def set_review_apps_uploads_path_in_initializer
+        gsub_file("config/initializers/decidim.rb", /^end/, <<-INITIALIZER_CONTENT)
+  if ENV["HEROKU_APP_NAME"].present?
+    config.base_uploads_path = ENV["HEROKU_APP_NAME"] + "/"
+  end
+end
+INITIALIZER_CONTENT
+      end
+
+      def set_seeds_for_review_apps
+        gsub_file("db/seeds.rb", "Decidim.seed!", <<-SEEDS_CONTENT)
+if ENV["HEROKU_APP_NAME"].present?
+  ENV["DECIDIM_HOST"] = ENV["HEROKU_APP_NAME"] + ".herokuapp.com"
+  ENV["SEED"] = true
+end
+Decidim.seed!
+        SEEDS_CONTENT
+      end
+
       private
 
       def app_name
