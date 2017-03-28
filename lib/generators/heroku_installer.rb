d91b5f1e1829126df660b86b55cf2a399f41a1c5
Marc Riera Casals <mrc2407@gmail.com>
2017-03-28 14:28:32 +0200

    Add production gems config

--- lib/generators/heroku_installer.rb
+++ lib/generators/heroku_installer.rb
@@ -28,6 +28,86 @@ Decidim.seed!
         SEEDS_CONTENT
       end
 
+      def add_production_gems
+        insert_into_file("Gemfile", <<-PRODUCTION_GEMS, after: "group :production do")
+  gem "dalli"
+  gem "sendgrid-ruby"
+  gem "newrelic_rpm"
+  gem "lograge"
+  gem "sentry-raven"
+  gem "sidekiq"
+        PRODUCTION_GEMS
+      end
+
+      def add_context_for_sentry
+        insert_into_file("app/controllers/decidim_controller.rb", <<-SENTRY_CONTEXT, after: "class DecidimController < ApplicationController")
+  before_action :set_raven_context
+
+  private
+
+  def set_raven_context
+    return unless Rails.application.secrets.sentry_enabled?
+    Raven.user_context({id: try(:current_user).try(:id)}.merge(session))
+    Raven.extra_context(params: params.to_unsafe_h, url: request.url)
+  end
+        SENTRY_CONTEXT
+      end
+
+      def enable_sentry_for_production
+        gsub_file("config/secrets.yml", "default: &default", <<-SENTRY_DEFAULT)
+default: &default
+  sentry_enabled: false
+        SENTRY_DEFAULT
+        gsub_file("config/secrets.yml", "smtp_authentication: \"plain\"", <<-SENTRY_DEFAULT_PROD)
+  smtp_authentication: "plain"
+  sentry_enabled: true
+        SENTRY_DEFAULT_PROD
+      end
+
+      def set_dalli_as_cache_store
+        gsub_file("config/environments/production.rb", "# config.cache_store = :mem_cache_store", <<-DALLI_CONFIG)
+  if ENV["MEMCACHEDCLOUD_SERVERS"].present?
+    config.cache_store = :dalli_store, ENV["MEMCACHEDCLOUD_SERVERS"].split(","), {
+      username: ENV["MEMCACHEDCLOUD_USERNAME"], password: ENV["MEMCACHEDCLOUD_PASSWORD"]
+    }
+  end
+        DALLI_CONFIG
+      end
+
+      def lograge_config
+        environment(nil, env: "production") do
+"  config.lograge.enabled = true
+  config.lograge.formatter = Lograge::Formatters::Json.new
+  config.lograge.custom_options = lambda do |event|
+    {
+      remote_ip: event.payload[:remote_ip],
+      params: event.payload[:params].except('controller', 'action', 'format', 'utf8'),
+      user_id: event.payload[:user_id],
+      organization_id: event.payload[:organization_id],
+      referer: event.payload[:referer],
+    }
+    end"
+        end
+      end
+
+      def sidekiq_config
+        environment(nil, env: "production") do
+          "config.active_job.queue_adapter = :sidekiq"
+        end
+        prepend_to_file("config/routes.rb", "require \"sidekiq/web\"")
+        route "authenticate :user, lambda { |u| u.roles.include?(\"admin\") } do
+    mount Sidekiq::Web => '/sidekiq'
+  end"
+      end
+
+      def add_procfile
+        template "procfile.erb", "Procfile"
+      end
+
+      def add_sidekiq_config
+        template "sidekiq_config.yml.erb", "config/sidekiq.yml"
+      end
+
       private
 
       def app_name
